fc01e16f78f6c546d36547c420a1726bd555d885
Rino Tovar <>
2023-03-28 10:18:22 +0200

    DB update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
+import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const user = useUser();
-  console.log(user);
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -19,6 +20,11 @@ const Home: NextPage = () => {
             {user.isSignedIn && <SignOutButton />}
           </div>
         )}
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
